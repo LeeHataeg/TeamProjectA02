95310098d179740369f58d5c8fe35eccd116b014
LeeHaTaeg <gkxor6457@gmail.com>
2023-10-16 21:00:42 +0900

    Add-Scripts, InputAction, Sprite

---
+++ TeamProject_10.13/Assets/_LHT/Scripts/Character/Player/StateMachines/PlayerBaseState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBaseState : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
