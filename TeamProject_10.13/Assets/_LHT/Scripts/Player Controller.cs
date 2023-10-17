59ed966fdfad4ce23f68405f53fd63c97f6617f6
LeeHaTaeg <gkxor6457@gmail.com>
2023-10-17 19:30:04 +0900

    [Fix]

--- TeamProject_10.13/Assets/_LHT/Scripts/Player Controller.cs
+++ TeamProject_10.13/Assets/_LHT/Scripts/Player Controller.cs
@@ -7,18 +7,29 @@ using UnityEngine.InputSystem.XR;
 
 public class PlayerController : MonoBehaviour
 {
-
-    [SerializeField] private float originSpeed;
+    private float speed;
+    [SerializeField] private float originSpeed = 4f;
     public float jumpForce;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
 
+    private static PlayerController instance;
+    public static PlayerController Instance { get { return instance; } }
+
     private void Awake()
     {
+        if (instance == null)
+            instance = this;
+
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Start()
+    {
+        speed = originSpeed;
     }
 
     private void Update()
@@ -57,15 +68,17 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Move Speed
-        float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        MovePlayer();
 
-        //Max Speed
-        if (rigid.velocity.x > originSpeed)
-            rigid.velocity = new Vector2(originSpeed, rigid.velocity.y);
-        else if (rigid.velocity.x < originSpeed * (-1))
-            rigid.velocity = new Vector2(originSpeed * (-1), rigid.velocity.y);
+        ////Move Speed
+        //float h = Input.GetAxisRaw("Horizontal");
+        //rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        ////Max Speed
+        //if (rigid.velocity.x > speed)
+        //    rigid.velocity = new Vector2(speed, rigid.velocity.y);
+        //else if (rigid.velocity.x < speed * (-1))
+        //    rigid.velocity = new Vector2(speed * (-1), rigid.velocity.y);
 
         //Landing
         //Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
@@ -83,4 +96,31 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
+
+    void MovePlayer()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rigid.velocity = new Vector3(h, 0, 0).normalized * speed * Time.deltaTime;
+    }
+
+    #region Player Speed Up
+    public void IncreaseSpeed(float currentRunningTime, float runningMultipleValue)
+    {
+        float changeSpeed = originSpeed * runningMultipleValue;
+        print(changeSpeed);
+
+        speed = changeSpeed;
+        // 증가한 속도를 원래 속도로 되돌리기 위한 코루틴 시작
+        StartCoroutine(ResetSpeedAfterTime(currentRunningTime, originSpeed));
+    }
+
+    private IEnumerator ResetSpeedAfterTime(float currentRunningTime, float originalSpeed)
+    {
+        yield return new WaitForSeconds(currentRunningTime);
+
+        // currentRunninTime 이후 원래 속도로 돌아감
+        speed = originalSpeed;
+    }
+    #endregion
 }
