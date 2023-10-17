54cb694f8e6b0ab6cac6d86b285b214be3360583
LeeHaTaeg <gkxor6457@gmail.com>
2023-10-18 01:05:36 +0900

    [Add]
    Heart_Sprite

--- TeamProject_10.13/Assets/_LHT/Scripts/Player Controller.cs
+++ TeamProject_10.13/Assets/_LHT/Scripts/Player Controller.cs
@@ -7,12 +7,18 @@ using UnityEngine.InputSystem.XR;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed;
-    [SerializeField] private float originSpeed = 4f;
+
+    public float maxSpeed;
+    float originSpeed;
     public float jumpForce;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
+
+    public float magnetTime = 3f;
+    public float magnetRange = 5f;
+    public float magnetPower = 10f;
+    private bool isMagneticActive = false;
 
     private static PlayerController instance;
     public static PlayerController Instance { get { return instance; } }
@@ -29,7 +35,7 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-        speed = originSpeed;
+        originSpeed = maxSpeed;
     }
 
     private void Update()
@@ -70,25 +76,15 @@ public class PlayerController : MonoBehaviour
     {
         MovePlayer();
 
-        ////Move Speed
-        //float h = Input.GetAxisRaw("Horizontal");
-        //rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-        ////Max Speed
-        //if (rigid.velocity.x > speed)
-        //    rigid.velocity = new Vector2(speed, rigid.velocity.y);
-        //else if (rigid.velocity.x < speed * (-1))
-        //    rigid.velocity = new Vector2(speed * (-1), rigid.velocity.y);
-
-        //Landing
-        //Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+        //landing
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
         if (rigid.velocity.y < 0)
         {
             RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
 
             if (rayHit.collider != null)
             {
-                
+
                 if (rayHit.distance < 0.8f)
                 {
                     anim.SetBool("IsJump", false);
@@ -96,12 +92,20 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
-
     void MovePlayer()
     {
         float h = Input.GetAxisRaw("Horizontal");
 
-        rigid.velocity = new Vector3(h, 0, 0).normalized * speed * Time.deltaTime;
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)
+        {
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        }
+        else if (rigid.velocity.x < maxSpeed * (-1))
+        {
+            rigid.velocity = new Vector2(maxSpeed * -1, rigid.velocity.y);
+        }
     }
 
     #region Player Speed Up
@@ -110,17 +114,72 @@ public class PlayerController : MonoBehaviour
         float changeSpeed = originSpeed * runningMultipleValue;
         print(changeSpeed);
 
-        speed = changeSpeed;
+        maxSpeed = changeSpeed;
         // 증가한 속도를 원래 속도로 되돌리기 위한 코루틴 시작
         StartCoroutine(ResetSpeedAfterTime(currentRunningTime, originSpeed));
     }
-
     private IEnumerator ResetSpeedAfterTime(float currentRunningTime, float originalSpeed)
     {
         yield return new WaitForSeconds(currentRunningTime);
-
+        print(currentRunningTime);
         // currentRunninTime 이후 원래 속도로 돌아감
-        speed = originalSpeed;
+        maxSpeed = originalSpeed;
+    }
+    #endregion
+
+    #region Player Magnet Effect
+    public void MagneticEffect()
+    {
+        if (!isMagneticActive)
+        {
+            StartCoroutine(ApplyMagneticEffectContinuously());
+        }
+    }
+
+    private IEnumerator ApplyMagneticEffectContinuously()
+    {
+        isMagneticActive = true;
+        float time = 0;
+
+        while (isMagneticActive)
+        {
+            // 플레이어의 위치
+            Vector2 playerPosition = transform.position;
+
+            // 모든 ItemData 오브젝트를 찾음
+            ItemData[] itemsInScene = FindObjectsOfType<ItemData>();
+
+            foreach (ItemData item in itemsInScene)
+            {
+                // 아이템과 플레이어 사이의 거리를 계산
+                float distanceToPlayer = Vector2.Distance(playerPosition, item.transform.position);
+
+                // 만약 아이템이 자석 효과 범위 내에 있다면
+                if (distanceToPlayer <= magnetRange)
+                {
+                    // 자석 효과 방향을 계산
+                    Vector2 forceDirection = (playerPosition - (Vector2)item.transform.position).normalized;
+
+                    // 아이템에 자석 효과를 적용
+                    Rigidbody2D itemRigidbody = item.GetComponent<Rigidbody2D>();
+                    if (itemRigidbody != null)
+                    {
+                        itemRigidbody.AddForce(magnetPower * forceDirection, ForceMode2D.Force);
+                    }
+                }
+            }
+
+            time += Time.deltaTime;
+
+            yield return null;
+
+            if (time > magnetTime)
+            {
+
+                isMagneticActive = false;
+                yield break;
+            }
+        }
     }
     #endregion
 }
