333b98ea235bc5be0256c11918db580de8c99b61
LeeHaTaeg <gkxor6457@gmail.com>
2023-10-17 17:35:38 +0900

    [Add]
    Player Controller.cs - 기본 움직임, 점프(수정필요)

--- TeamProject_10.13/Assets/_LHT/Scripts/Player Controller.cs
+++ TeamProject_10.13/Assets/_LHT/Scripts/Player Controller.cs
@@ -1,26 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
 
 public class PlayerController : MonoBehaviour
 {
-    [field: Header("Reference")]
-    [field: SerializeField] public PlayerSO Data { get; private set; }
+    [SerializeField] private float originSpeed;
+    public float jumpForce;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
 
-    [field: Header("Animations")]
-    [field: SerializeField] public AnimationData AnimationData { get; private set; }
-
-    public Rigidbody Rigidbody { get; private set; }
-    public Animator Animator { get; private set; }
-    public PlayerInput Input { get; private set; }
     private void Awake()
     {
-        AnimationData.Initialize();
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();
+    }
 
-        Rigidbody = GetComponent<Rigidbody>();
-        Animator = GetComponentInChildren<Animator>();
-        Input = GetComponent<PlayerInput>();
+    private void Update()
+    {
+        Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, transform.position.z), Vector2.down * 0.8f);
+        //Jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("IsJump"))
+        {
+            rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetBool("IsJump", true);
+        }
+
+        //Stop Speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        //Direction Sprite
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        //
+        if (rigid.velocity.normalized.x == 0)
+        {
+            anim.SetBool("IsWalk", false);
+        }
+        else
+        {
+            anim.SetBool("IsWalk", true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Move Speed
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        //Max Speed
+        if (rigid.velocity.x > originSpeed)
+            rigid.velocity = new Vector2(originSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < originSpeed * (-1))
+            rigid.velocity = new Vector2(originSpeed * (-1), rigid.velocity.y);
+
+        //Landing
+        //Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+        if (rigid.velocity.y < 0)
+        {
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                
+                if (rayHit.distance < 0.8f)
+                {
+                    anim.SetBool("IsJump", false);
+                }
+            }
+        }
     }
 }
